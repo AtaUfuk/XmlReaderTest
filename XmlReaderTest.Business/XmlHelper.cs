2cb7b9295743a1a75944e99feaeee2b3ce10946b
Ufuk Ata <45665853+AtaUfuk@users.noreply.github.com>
2022-04-01 05:13:47 +0300

    Added Proxy Test

--- XmlReaderTest.Business/XmlHelper.cs
+++ XmlReaderTest.Business/XmlHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
@@ -7,13 +8,13 @@ namespace XmlReaderTest.Business
 {
     public class XmlHelper
     {
-        public TResponse GetXmlResponse<TRequest, TResponse>(TRequest request, string url, HttpMethod httpMethod,Encoding encoding, Dictionary<string, string>? customHeaders,int timeOut=0) 
+        public TResponse GetXmlResponse<TRequest, TResponse>(TRequest request, string url, HttpMethod httpMethod, Encoding encoding, Dictionary<string, string>? customHeaders, int timeOut = 0)
             where TRequest : class where TResponse : class, new()
         {
             TResponse? response = null;
             try
             {
-                using (HttpClient httpClient=new HttpClient())
+                using (HttpClient httpClient = new HttpClient())
                 {
                     if (timeOut > 0)
                     {
@@ -31,21 +32,21 @@ namespace XmlReaderTest.Business
                     XmlSerializer xmlSerializer = new XmlSerializer(typeof(TRequest));
                     using (MemoryStream writer = new MemoryStream())
                     {
-                        XmlWriterSettings settings=new XmlWriterSettings();
-                        settings.Encoding= encoding;
+                        XmlWriterSettings settings = new XmlWriterSettings();
+                        settings.Encoding = encoding;
                         settings.Indent = false;
-                        using (XmlWriter xmlWriter = XmlWriter.Create(writer,settings))
+                        using (XmlWriter xmlWriter = XmlWriter.Create(writer, settings))
                         {
                             xmlSerializer.Serialize(xmlWriter, request);
                             string xml = encoding.GetString(writer.ToArray());
-                            requestMessage.Content = new StringContent(xml, encoding,"text/xml");
+                            requestMessage.Content = new StringContent(xml, encoding, "text/xml");
                         }
                     }
 
                     var xmlResponse = httpClient.Send(requestMessage);
                     if (xmlResponse.StatusCode == System.Net.HttpStatusCode.OK)
                     {
-                        using (StringReader strReader=new StringReader(xmlResponse.Content.ReadAsStringAsync().Result))
+                        using (StringReader strReader = new StringReader(xmlResponse.Content.ReadAsStringAsync().Result))
                         {
                             XmlSerializer xmlDeSerializer = new XmlSerializer(typeof(TResponse));
                             response = xmlDeSerializer.Deserialize(strReader) as TResponse;
@@ -55,10 +56,73 @@ namespace XmlReaderTest.Business
             }
             catch (Exception ex)
             {
-
+                //Add Your Log Method here
                 throw;
             }
+#pragma warning disable CS8603 // Possible null reference return.
             return response;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        public TResponse GetXmlResponseWithProxy<TRequest, TResponse>(TRequest request, string url, HttpMethod httpMethod, Encoding encoding, HttpClientHandler clientHandler, Dictionary<string, string>? customHeaders, int timeOut = 0)
+            where TRequest : class where TResponse : class, new()
+        {
+            TResponse? response = null;
+            try
+            {
+                if(object.Equals(clientHandler,null))
+                {
+                    throw new ArgumentNullException("Client Handler can not be null!!!");
+                }
+
+                using (HttpClient httpClient = new HttpClient(clientHandler))
+                {
+                    if (timeOut > 0)
+                    {
+                        httpClient.Timeout = TimeSpan.FromSeconds(timeOut);
+                    }
+                    if (customHeaders != null && customHeaders.Any())
+                    {
+                        httpClient.DefaultRequestHeaders.Clear();
+                        foreach (var item in customHeaders)
+                        {
+                            httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                        }
+                    }
+                    HttpRequestMessage requestMessage = new HttpRequestMessage(httpMethod, url);
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(TRequest));
+                    using (MemoryStream writer = new MemoryStream())
+                    {
+                        XmlWriterSettings settings = new XmlWriterSettings();
+                        settings.Encoding = encoding;
+                        settings.Indent = false;
+                        using (XmlWriter xmlWriter = XmlWriter.Create(writer, settings))
+                        {
+                            xmlSerializer.Serialize(xmlWriter, request);
+                            string xml = encoding.GetString(writer.ToArray());
+                            requestMessage.Content = new StringContent(xml, encoding, "text/xml");
+                        }
+                    }
+
+                    var xmlResponse = httpClient.Send(requestMessage);
+                    if (xmlResponse.StatusCode == HttpStatusCode.OK)
+                    {
+                        using (StringReader strReader = new StringReader(xmlResponse.Content.ReadAsStringAsync().Result))
+                        {
+                            XmlSerializer xmlDeSerializer = new XmlSerializer(typeof(TResponse));
+                            response = xmlDeSerializer.Deserialize(strReader) as TResponse;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Add Your Log Method here
+                throw;
+            }
+#pragma warning disable CS8603 // Possible null reference return.
+            return response;
+#pragma warning restore CS8603 // Possible null reference return.
         }
     }
 }
--- XmlReaderTest.UI/Program.cs
+++ XmlReaderTest.UI/Program.cs
@@ -1,22 +1,49 @@
-﻿using XmlReaderTest.Business;
+﻿using System.Net;
+using XmlReaderTest.Business;
 using XmlReaderTest.UI.Models.NumberToDollar;
 // See https://aka.ms/new-console-template for more information
 Console.Write("Lütfen Bir Sayı Giriniz:");
-string dolar = Console.ReadLine();
-Console.WriteLine("Sonuç:"+GetResult(dolar));
+string dolar = Console.ReadLine() ?? "10";
+Console.WriteLine("Sonuç:" + GetResult(dolar));
 Console.ReadKey();
 string GetResult(string input)
 {
-    NumberToDollars request=new NumberToDollars();
+    NumberToDollars request = new NumberToDollars();
     request.dNum = decimal.Parse(input);
-    Envelope envelope=new Envelope();
-    Body body=new Body();
+    Envelope envelope = new Envelope();
+    Body body = new Body();
     body.NumberToDollars = request;
-    envelope.Body=body;
+    envelope.Body = body;
     XmlHelper xmlHelper = new XmlHelper();
     Dictionary<string, string> data = new Dictionary<string, string>();
     data.Add("Accept", "text/xml; charset=utf-8");
-    var result= xmlHelper.GetXmlResponse<Envelope, ResponseEnvelope>(envelope, "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?op=NumberToDollars", HttpMethod.Post,System.Text.Encoding.UTF8, data);
+    var result = xmlHelper.GetXmlResponse<Envelope, ResponseEnvelope>(envelope, "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?op=NumberToDollars", HttpMethod.Post, System.Text.Encoding.UTF8, data);
+
+    return result.Body.NumberToDollarsResponse.NumberToDollarsResult;
+}
+
+string GetResultProxy(string input)
+{
+    NumberToDollars request = new NumberToDollars();
+    request.dNum = decimal.Parse(input);
+    Envelope envelope = new();
+    Body body = new()
+    {
+        NumberToDollars = request
+    };
+    envelope.Body = body;
+    XmlHelper xmlHelper = new();
+    Dictionary<string, string> data = new Dictionary<string, string>();
+    data.Add("Accept", "text/xml; charset=utf-8");
+    WebProxy proxy = new()
+    {
+        Address = new Uri("https://192.168.1.1:3131")
+    };
+    HttpClientHandler clientHandler = new();
+    clientHandler.Proxy = proxy;
+    clientHandler.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+    clientHandler.AllowAutoRedirect = true;
+    var result = xmlHelper.GetXmlResponseWithProxy<Envelope, ResponseEnvelope>(envelope, "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?op=NumberToDollars", HttpMethod.Post, System.Text.Encoding.UTF8, clientHandler, data);
 
     return result.Body.NumberToDollarsResponse.NumberToDollarsResult;
 }
